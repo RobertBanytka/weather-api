c3a988cd716526c2480156a6cb7e540fa88ddc6a
RobertBanytka <banytkarobert@gmail.com>
2026-04-07 13:53:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-{
-  "name": "weather-api",
-  "type": "module",
-  "dependencies": {
-    "express": "^4.18.2",
-    "node-fetch": "^3.3.2"
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = req.query;
+
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}`+
+    `&current=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m,surface_pressure,precipitation,weather_code,uv_index`+
+    `&hourly=temperature_2m,precipitation_probability,precipitation,weather_code,wind_speed_10m,uv_index,cape`+
+    `&forecast_days=2&wind_speed_unit=kmh&timezone=auto`;
+
+  try {
+    const r = await fetch(url);
+    const data = await r.json();
+    res.json(data);
+  } catch {
+    res.status(500).json({ error: "API error" });
   }
-}
+});
+
+app.listen(3000, () => console.log("API działa"));
